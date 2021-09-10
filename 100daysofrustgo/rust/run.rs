79255e09d764baff9b054d0b901c256aee0c125c
Alex Merced <alex@alexmerced.dev>
2021-09-10 09:50:55 -0400

    day 17

--- 100daysofrustgo/rust/run.rs
+++ 100daysofrustgo/rust/run.rs
@@ -1,14 +1,27 @@
 // use std::collections::HashMap;
 // use std::fmt::Error;
 // use std::env;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
 
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:5000").unwrap();
 
-fn main (){
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
 
-    let mut my_string:String = String::from("Hello, it's me, Mario");
+        handle_connection(stream);
+    }
+}
 
-    my_string.shrink_to(2);
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
 
-    println!("{}", my_string);
+    stream.read(&mut buffer).unwrap();
 
+    let response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n'Hello World'\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
