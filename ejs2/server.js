2d5afd6c87eb322e41d79802198dc1f5290583c2
Alex Merced <alex@alexmerced.dev>
2022-12-08 10:50:00 -0500

    update

--- ejs2/server.js
+++ ejs2/server.js
@@ -8,12 +8,30 @@ const app = express();
 // changes the views folder
 // app.set("views", process.cwd() + "/views2")
 
-app.get("*", (req, res) => {
-  res.render("index.ejs");
-});
+
 
 app.post("/clicked", (req, res) => {
     res.send("<h1>You clicked the button</h1>")
 })
+
+app.get("/senddata", (req, res) => {
+  const obj = {message: "Hello World"}
+
+  res.render("senddata.ejs", {obj})
+
+})
+
+app.get("/senddata2", (req, res) => {
+  const obj = {message: "Hello World"}
+
+  res.json(obj)
+
+})
+
+
+
+app.get("*", (req, res) => {
+  res.render("index.ejs");
+});
 
 app.listen(3000, () => console.log("Listening on 3000"));
